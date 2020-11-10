243d88129ff614fc13d4fbbba19bc4d8107421dc
Brady Nahkala <58004263+bnahkala@users.noreply.github.com>
2020-11-10 15:08:42 -0600

    commenting
    clarifying code

--- Alternative Scenario Batch.py
+++ Alternative Scenario Batch.py
@@ -3,13 +3,16 @@
 # LAST REVISED: 09 MAR 2020
 # PURPOSE: Using a database of alternative management files, cycle through 
 # pothole management scenarios (ref 1) for multiple potholes. 
-# Utilized 03.09.2020 to add scen 18-21
 #
-# REF 1: C:\AGNPS_Watershed_Studies\Model Development Manager.xlsx
+# Personal Reference: C:\AGNPS_Watershed_Studies\Model Development Manager.xlsx
+#
+# LAST CLEANED: 11 NOV 2020
+# Commenting code. 
 #
 # =======================================================================
 #
-# IMPORT =============================
+#
+# LIBRARY =============================
 import os
 import platform
 import sys
@@ -20,20 +23,22 @@ import scipy
 
 
 # METADATA ===========================
-pothole_names = ["Bunny"]
-scenario_numerical = [0, 1, 2]
-scenario_name = ["NA"]
+pothole_names = ["Bunny"] # unique identifier
+scenario_numerical = [0, 1, 2] # any number of scenarios you've built and want to run, listed here
+scenario_name = ["NA"] # can give each scenario a name, for reference, as shown in examples below
 #"Base", "Retired-All", "Conservation Tillage", "Retired-Pothole", "Improved Drainage 01","Improved Drainage 02", "Improved Drainage 03", 
 # "Planting Date 01", "Planting Date 02", "Planting Date 03", "Planting Date 04", 
 # "No-Till", "ConTillRetired", "NTillRetired", "ConTillDrain1", "ConTillDrain2", "ConTillDrain3", "Cover Crop"
 # "BaseRetired", Improved Drainage 01 Retired","Improved Drainage 02 Retired", "Improved Drainage 03 Retired"
 
-wetland_area_ha = 5.35   # from calibration, selected Method 4, MaxA delineation
-wetland_area_m2 = wetland_area_ha * 10000
+wetland_area_ha = 5.35   # from calibration, in hectares
+wetland_area_m2 = wetland_area_ha * 10000 # square meters
 
+# UNUSED ===
 # d-A-V is linear below 0.1 m and second-order above 0.1 m
 regression_transition_m = 0.1
 regression_transition_m3 = 266.7
+ # =========
 
 # DATES and data array
 # gregorian day for start and end of simulation (1994-2018, excluding 2-year spinup)
@@ -41,26 +46,33 @@ start = 727929
 end = 737059
 duration = end - start + 2
 
-volume_data = numpy.empty([duration, 45])
+volume_data = numpy.empty([duration, 45]) # y value needs to be big enough to hold number of scenarios
 depth_data = numpy.empty([duration, 45])
 
-z = 0
+z = 0 # pothole id
 for x in pothole_names:
     # POTHOLE FILE NAMES
+    
+    # controls simulation and aggregates inputs for AnnAGNPS
     master = "C:/AGNPS_Watershed_Studies/" + str(pothole_names[z]) + "_Alternatives/4_Editor_Datasets/CSV_Input_Files/annagnps_master.csv"
+    
+    # Output file for copying volumetric time series from each simulation run
     output_file = "C:/AGNPS_Watershed_Studies/Output_Database/"+ str(pothole_names[z]) + "/" + str(pothole_names[z]) + "_Output.csv"
+    
+    # AnnAGNPS generated output file after each run
     wetSIM_filename = "C:/AGNPS_Watershed_Studies/" + str(pothole_names[z]) + "_Alternatives/5_AnnAGNPS_DataSets/AnnAGNPS_SIM_Wetland_Effects.csv"
-    for y in scenario_numerical:
+    
+    for y in scenario_numerical: # run each scenario listed above
         # SCENARIO FILE NAMES
         header = "Data Section ID, File Name"
         annID = 'AnnAGNPS ID,.\simulation\\annaid.csv'
-        cell = 'Cell Data,.\watershed\AnnAGNPS_Cell_Data_Section_' + str(y) + '.csv'
+        cell = 'Cell Data,.\watershed\AnnAGNPS_Cell_Data_Section_' + str(y) + '.csv' # increment which cell file is used, land management data changed
         crop = "Crop Data,.\general\cropdata_RUSLE1.csv"
         cropg = "Crop Growth Data,.\general\crop_growth_RUSLE1.csv"
         glob = "Global IDs Factors and Flags Data,.\simulation\globfac.csv"
-        mngf = "Management Field Data,.\general\AnnAGNPS_Management_Field_Data_" + str(y) + ".csv"
+        mngf = "Management Field Data,.\general\AnnAGNPS_Management_Field_Data_" + str(y) + ".csv" # incrememnt management field data, becuase it changed (could have a single file with all possible schedules for your simulations)
         mngo = "Management Operation Data,.\general\AnnAGNPS_Management_Operation_Data_Template.csv"
-        mngs = "Management Schedule Data,.\general\AnnAGNPS_Management_Schedule_Data_" + str(y) + ".csv"
+        mngs = "Management Schedule Data,.\general\AnnAGNPS_Management_Schedule_Data_" + str(y) + ".csv" # same as management field data
         ncdat = "Non-Crop Data,.\general\AnnAGNPS_Non_Crop_Data_Section_2.csv"
         rch = "Reach Data,.\watershed\AnnAGNPS_Reach_Data_Section.csv"
         rcn = "Runoff Curve Number Data,.\general\Runoff_Curve_Number_Data_Input_Editor_v5.5_Format.csv"
@@ -79,8 +91,9 @@ for x in pothole_names:
         oomm = "Output Options - MN/MX,.\simulation\outopts_limits.csv"
         climst = "CLIMATE DATA - STATION,.\climate\climate_station.csv" 
         climd = "CLIMATE DATA - DAILY,.\climate\climate_daily.csv"
-        wetland = "Wetland Data,.\watershed\AnnAGNPS_Wetland_Data_Section_" + str(y) + ".csv" 
+        wetland = "Wetland Data,.\watershed\AnnAGNPS_Wetland_Data_Section_" + str(y) + ".csv" # change wetland file for changes in drainage
 
+        # write file references to master
         names = [header, annID, cell, crop, cropg, glob, mngf, mngo, mngs, ncdat, rch, rcn, sim, soil, soill, shed, oog, ooaa, oocsv, oodpp, ooev, oonpt, oosim, ootbl, oomm, climst, climd, wetland]
         w = open(master, "w").close()
         w = open(master, "w")
@@ -91,7 +104,7 @@ for x in pothole_names:
             n = n + 1
         w.close()
 
-        # BATCH FILES
+        # BATCH FILES - name them and specify directory
         name_batchdelete = "C:/AGNPS_Watershed_Studies/" + str(pothole_names[z]) + "_Alternatives/0_Batch_files/0_delete_all_output_files_BAN.bat"
         name_exannagnps = "C:/AGNPS_Watershed_Studies/" + str(pothole_names[z]) + "_Alternatives/0_Batch_files/3_execute_AnnAGNPS_BAN.bat"
 
@@ -102,17 +115,17 @@ for x in pothole_names:
         subprocess.run(name_exannagnps)
         
         # EXTRACT WETLAND SIM DATA
-        # copy output file
+        # copy entire output file
         destination_filename = "C:/AGNPS_Watershed_Studies/Output_Database/" + str(pothole_names[z]) + "/AnnAGNPS_SIM_Wetland_Effects_" + str(pothole_names[z]) + "_" + str(y) + ".csv"
         shutil.copyfile(wetSIM_filename, destination_filename)
         
         #read output file
         wetread = numpy.genfromtxt(fname = wetSIM_filename, delimiter=',', usecols=numpy.arange(0,32), invalid_raise=False)
         
-        # APPEND ALL SIM DATA
+        # APPEND ALL SIM DATA to output_file location, for comparing simulations
         i = 4
         j = 0
-        for m in range(11, 9865):
+        for m in range(11, 9865): # change based on date range
             if (wetread[m, 0] >= start and wetread[m, 0] <= end):
                 volume_data[j, 0] = wetread[m, 0] 
                 volume_data[j, 1] = wetread[m, 3]
@@ -134,6 +147,6 @@ for x in pothole_names:
 
                 m = m + 1
                 j= j + 1 
-        numpy.savetxt(output_file, volume_data, delimiter=",")
+        numpy.savetxt(output_file, volume_data, delimiter=",") # save csv of time series from all model runs
     z = z + 1
 
